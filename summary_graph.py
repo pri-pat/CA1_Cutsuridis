b824ffeecc777c7a30268b2d6498b54aae23bacb
Marianne Bezaire <mariannejcase@gmail.com>
2020-08-11 13:26:08 -0400

    summary stats

--- summary_graph.py
+++ summary_graph.py
@@ -5,9 +5,6 @@ Created on Tue Aug 11 10:30:39 2020
 @author: mbezaire
 """
 
-
-import pickle
-
 runs2include=["par","sim1"]
 
 performance_list=[]
@@ -15,17 +12,23 @@ death_list=[]
 electrostim_list=[]
 
 for run in runs2include:
-    with open('pyresults/'+ run+'.pkl') as f:  # Python 3: open(..., 'rb')
-        spikeout, vout, perf, data2save = pickle.load(f)
-        performance_list.append(data2save["performance"])
-        death_list.append(data2save["percentDeath"])
-        electrostim_list.append(data2save["electrostim"])
+    with open('pyresults/' + run+'_performance.txt', 'r') as f:  # Python 3: open(..., 'wb')
+        content = f.readlines()
+    
+        perf = float(content[0])
+        electrostim = float(content[1])
+        percentDeath = float(content[2])
+
+        performance_list.append(perf)
+        death_list.append(electrostim)
+        electrostim_list.append(percentDeath)
         
 import matplotlib.pyplot as plt
 
 plt.figure()
-plt.plot(death_list,performance_list)
+plt.plot(death_list,performance_list,'ro')
 plt.xlabel('% Cell Death')
 plt.ylabel('Memory Recall Performance (Scale of 0 to 1)')
 plt.show()
 plt.savefig('Images/memory_recall_v_death.png')
+
